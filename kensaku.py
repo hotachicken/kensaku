f212f59b4fdd24bd85912466fbdf88b282057092
unknown <rhomatt@gmail.com>
2020-05-10 02:56:59 -0400

    it actually prints the damn thing finally

--- kensaku.py
+++ kensaku.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageGrab
-import pytesseract
+import pytesseract as ptess
+import sys
 
 """
 Gets contents of clipboard. If an image, get as an image.
@@ -7,3 +8,16 @@ For now, just assume an image
 """
 def get_from_clip():
     img = ImageGrab.grabclipboard()
+    if isinstance(img, Image.Image):
+        img.save('temp.png')
+    else:
+        print("no image found", flush=True)
+    isimage = True
+    return img, isimage
+
+if __name__ == "__main__":
+    img, isimage = get_from_clip()
+    if isimage:
+        out = ptess.image_to_string(Image.open('temp.png'), lang='jpn', config='--psm 6')
+        out = out.encode('utf8')
+        sys.stdout.buffer.write(out)
