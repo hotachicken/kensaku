2e9ac14e3546bdc484bda748cce0c74a54e2c95e
unknown <rhomatt@gmail.com>
2020-05-11 22:33:24 -0400

    remove whitespace

--- kensaku.py
+++ kensaku.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageGrab
 import pytesseract as ptess
 import pyperclip
 import sys
+import re
 
 """
 Gets contents of clipboard. If an image, get as an image.
@@ -23,6 +24,7 @@ if __name__ == "__main__":
     img, isimage = get_from_clip()
     if isimage:
         out = ptess.image_to_string(img, lang='jpn', config='--psm 6')
+        out = re.sub('\\s', '', out)
         pyperclip.copy(out)
         out = out.encode('utf8')
         sys.stdout.buffer.write(out)
